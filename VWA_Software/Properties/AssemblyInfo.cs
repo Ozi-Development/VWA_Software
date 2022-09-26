062ce64229b1d60ea31562a975c884eee7c04658
Elias Valentek <elias.valentek@chello.at>
2022-09-26 19:56:18 +0200

    Release Prepare

--- VWA_Software/Properties/AssemblyInfo.cs
+++ VWA_Software/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("VWA_Software")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Programm zur Auswahl von Wahlpflichtfächern")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VWA_Software")]
@@ -51,6 +51,6 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("de")]
